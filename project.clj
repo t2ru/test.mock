883527670181c596adb027b01358e9e3eaf98c5d
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2012-02-18 19:22:22 +0900

    import

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.taka2ru/test.mock "0.0.1-SNAPSHOT"
+  :description "clojure.contrib.mock for 1.3"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.incubator "0.1.0"]])
---
+++ test/mock/test/core.clj
@@ -0,0 +1,6 @@
+(ns mock.test.core
+  (:use [mock.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
